5ddd44e1e8680f98a49f78254bbd802567dec226
Veronika <n.kanchura.dodo@gmail.com>
2021-03-06 22:31:37 +0400

    jv'

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int sum = 2_000_000_000;
+        int transfer = 500_000_000;
+        int total = sum + transfer ;
+        System.out.println(total);
+    }
+}
